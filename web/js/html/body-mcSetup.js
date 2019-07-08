402a39c77e45c9353596ab313b9f24ee4d3c008a
Xestra1 <33702536+Xestra1@users.noreply.github.com>
2019-07-07 23:08:16 -0400

    did js-html thing I dont know what to call

---
+++ web/js/html/body-mcSetup.js
@@ -0,0 +1,158 @@
+const body = `    
+<div class="body">
+<div class="container">
+    <div class="project">
+        <br>
+        <!-- <img src="/img/undraw_gaming_6oy3.svg" alt=""> -->
+        <div class="desc">
+            <h3>Factions Setup</h3>
+            <p>Our factions setup uses a bunch of optimized plugins like SavageFactions, BeerSpigot, ShopGUI+,
+                and many more. In addition to the other plugins, we also use custom plugins made by our
+                developers. We also spend a lot of time making the perfect economy and perfect spigot configs.
+            </p>
+            <div class="card-deck">
+                <div class="card">
+                    <h6><a class="fas fa-bullseye"></a> Perfect Cannoning</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-microchip"></a> Many Features</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-tachometer-alt"></a> 19.75 TPS Guarantee</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-clipboard"></a> Custom Plugins</h6>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <hr>
+    <div class="project">
+        <br>
+        <!-- <img src="/img/undraw_gaming_6oy3.svg" alt=""> -->
+        <div class="desc">
+            <h3>Skyblock Setup</h3>
+            <p>Our skyblock setup provides a unique experience and insane performance increases as well. We use
+                a wide range of plugin to complete the setup and we use custom plugins too.</p>
+            <div class="card-deck">
+                <div class="card">
+                    <h6><a class="fas fa-money-bill"></a> Perfect Economy</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-wrench"></a> Spawner Based Economy</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-sync"></a> Async Skyblock Generation</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-tachometer-alt"></a> 19.75 TPS Guarantee</h6>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <hr>
+    <div class="project">
+        <br>
+        <!-- <img src="/img/undraw_gaming_6oy3.svg" alt=""> -->
+        <div class="desc">
+            <h3>Survival Setup</h3>
+            <p>Our survival setup provides fun and enjoyable game play for everyone. With our perfect economy
+                and custom worlds almost everyone will enjoy our survival setup.</p>
+            <div class="card-deck">
+                <div class="card">
+                    <h6><a class="fas fa-money-bill"></a> Perfect Economy</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-tractor"></a> Farming Based Economy</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-tachometer-alt"></a> Latest Version</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-globe-americas"></a> Custom World</h6>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <hr>
+    <div class="project">
+        <br>
+        <!-- <img src="/img/undraw_gaming_6oy3.svg" alt=""> -->
+        <div class="desc">
+            <h3>DrugWars Setup</h3>
+            <p>Although factions is fun, DrugWars is different and enjoyable. Our setup turns factions into a
+                farming based economy and its mainly setup to be more like OG factions.</p>
+            <div class="card-deck">
+                <div class="card">
+                    <h6><a class="fas fa-tractor"></a> Farming Based Economy</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-money-bill"></a> Balanced Economy</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-clipboard"></a> Custom Plugins</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-folder-open"></a> OG Factions</h6>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <hr>
+    <div class="project">
+        <br>
+        <!-- <img src="/img/undraw_gaming_6oy3.svg" alt=""> -->
+        <div class="desc">
+            <h3>HCF Setup</h3>
+            <p>Hard core factions or HCF is a enjoyable pvp setup with many features. With a perfect pvp spigot,
+                all players will enjoy HCF and have a lagless experience.</p>
+            <div class="card-deck">
+                <div class="card">
+                    <h6><a class="fas fa-shield-alt"></a> HCF PvP</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-tools"></a> Perfect PvP Spigot</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-toolbox"></a> Custom Plugins</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-wallet"></a> Economy Setup</h6>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <hr>
+    <div class="project">
+        <br>
+        <!-- <img src="/img/undraw_gaming_6oy3.svg" alt=""> -->
+        <div class="desc">
+            <h3>Minigames Setup</h3>
+            <p>Quick and fun games with friends is a perfect way of bring players to your server. With a high
+                tps setup, almost no one will lag!</p>
+            <div class="card-deck">
+                <div class="card">
+                    <h6><a class="fas fa-dice"></a> Bunch of Minigames</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-tachometer-alt"></a> 19.75 TPS Games</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-toolbox"></a> Custom Plugins</h6>
+                </div>
+                <div class="card">
+                    <h6><a class="fas fa-tools"></a> Custom Skripts</h6>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+</div>
+</div>
+`;
+document.getElementById("body").innerHTML += body;
---
+++ web/js/html/navbar-inFile.js
@@ -0,0 +1,46 @@
+const navbar = `    
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<div class="container">
+    <h6 class="navbar-brand" href="#">OrsTac</h6>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="../index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../about.html">About</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Development
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <h6 class="dropdown-header">Website Development</h6>
+                    <a class="dropdown-item" href="./javascript.html">JavaScript</a>
+                    <!-- <a class="dropdown-item" href="./react.html">React</a> -->
+                    <a class="dropdown-item" href="./bootstrap.html">Bootstrap Themes</a>
+                    <div class="dropdown-divider"></div>
+                    <h6 class="dropdown-header">Server Setup</h6>
+                    <a class="dropdown-item active" href="./mc-server-setup.html">MC Server
+                        Setup</a>
+                    <div class="dropdown-divider"></div>
+                    <h6 class="dropdown-header">Commissions</h6>
+                    <a class="dropdown-item" href="./websites.html">Websites</a>
+                    <!-- <a class="dropdown-item" href="#">Server Setup</a> -->
+                </div>
+            </li>
+            <li class="nav-item">
+                <!-- <a class="nav-link" href="../contact.html">Contact</a> -->
+            </li>
+        </ul>
+        <a class="btn btn-primary" href="https://discord.gg/SsS5VcS">Support</a>
+    </div>
+</div>
+</nav>
+`;
+document.getElementById("navbar").innerHTML += navbar;
